2ab09b18eecb0263b3110aa318b4c2b4d1135240
Humberto Rondon <therealhjrp@gmail.com>
2021-10-31 11:12:33 -0400

    Update init.el

--- init.el
+++ init.el
@@ -1,9 +1,41 @@
-;;; early-init.el --- Emacs package managers configuration -*- lexical-binding: t; -*-
+;; -*- lexical-binding: t; -*-
+
+(defmacro chaos-toggle-mode (on/off &rest modes)
+  "Turns ON/OFF the specified MODES."
+  (declare (indent defun))
+  (if (string= (symbol-name on/off) "off")
+      `(progn
+	 ,@(mapcar (lambda (mode) `(,mode -1))
+		   modes))
+    `(progn
+       ,@(mapcar (lambda (mode) `(,mode 1))
+		 modes))))
+
+(chaos-toggle-mode off
+  menu-bar-mode
+  tool-bar-mode
+  scroll-bar-mode)
+
+(chaos-toggle-mode on
+  ido-mode
+  global-visual-line-mode)
+
+(defmacro chaos-global-set-key (key proc)
+  `(global-set-key (kbd ,key) ',proc))
+
+(chaos-global-set-key "M-1" delete-other-windows)
+(chaos-global-set-key "M-2" split-window-below)
+(chaos-global-set-key "M-3" split-window-right)
+(chaos-global-set-key "M-0" delete-window)
+
+(defmacro chaos-hook (hook &rest procs)
+  `(add-hook ',(intern (concat (symbol-name hook) "-hook"))
+	     (lambda ()
+	       (progn
+		 ,@procs))))
+
+(chaos-hook prog-mode
+	    (setq display-line-numbers 'relative))
 
 
-;; Code
-
-(setq package-enable-at-startup nil)
-
-
-;;; early-init.el ends here
+;;; init.el ends here
